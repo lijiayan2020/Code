089d8052624365dabe14bbfc83d7415c15afb1e1
lijiayan2020 <61346676+lijiayan2020@users.noreply.github.com>
2021-02-05 13:08:05 +0800

    SuanFa第一章

---
+++ Python/numpy_GuanFang_File/understand_np.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+my_array = np.array([1, 2, 3, 4, 5])
+print(my_array)
+
+print(my_array.shape)
+
+my_new_array = np.zeros((5))
+print(my_new_array) 
+
+another_array = np.ones((5))
+print(another_array)
+
+my_random_array = np.random.random((5))
+print(my_random_array)
+
+my_2d_array = np.ones((2,4))
+print(my_2d_array)
+
+my_array = np.array([[4, 5], [6, 1]])
+print(my_array)
+print(my_array.shape)
